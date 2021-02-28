1ad8aad77277c2db630e856041c58f6b666a8988
satyamrathor <78701910+satyamrathor@users.noreply.github.com>
2021-02-28 16:30:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+var app =http.createServer();
+
+app.listen(5000,()=>{
+    console.log("Server started");
+})
+
+
+
+
+
+// var express = require('express');
+
+// const app = express();
+
+// app.listen(5000,()=>{
+//     console.log("Server started");
+// })
